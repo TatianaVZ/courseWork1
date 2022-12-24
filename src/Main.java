d1da7509a7298746538c74ec1a8fcd3f8faf871a
TatianaVZ <ms.zadvorina@bk.ru>
2022-12-24 19:01:37 +1100

    Курсовую работу исправила по замечаниям.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 public class Main {
     public static void main(String[] args) {
         Salary salary = new Salary();
-        Name[] name = new Name[5];
-        name[0] = new Name("Иванов", "Иван", "Иванович", 55000);
-        name[1] = new Name("Семенов", "Семен", "Семенович", 65000);
-        name[2] = new Name("Александров", "Алесандр", "Александрович", 45000);
-        name[3] = new Name("Петров", "Петр", "Петрович", 75000);
-        name[4] = new Name("Борисов", "Борис", "Борисович", 95000);
-        for (Name i : name) {
+        Employee[] employee = new Employee[5];
+        employee[0] = new Employee("ДКС", 1, "Иванов", "Иван", "Иванович", 55000);
+        employee[1] = new Employee("ДКС", 2,"Семенов", "Семен", "Семенович", 65000);
+        employee[2] = new Employee("ДКС", 3,"Александров", "Алесандр", "Александрович", 45000);
+        employee[3] = new Employee("ДКС", 4,"Петров", "Петр", "Петрович", 75000);
+        employee[4] = new Employee("ДКС", 5,"Борисов", "Борис", "Борисович", 95000);
+        for (Employee i : employee) {
             System.out.println(i);
 
         }
 
         {
-            System.out.println("Общая сумма затрат на зарплаты сотрудникам: " + salary.totalSum(name) + " руб.");
+            System.out.println("Общая сумма затрат на зарплаты сотрудникам: " + salary.totalSum(employee) + " руб.");
         }
         {
-            System.out.println("Максимальная зарплата: " + salary.maxSalary(name) + " руб.");
+            System.out.println("Максимальная зарплата: " + salary.maxSalary(employee) + " руб.");
         }
         {
-            System.out.println("Минимальная зарплата: " + salary.minSalary(name) + " руб.");
+            System.out.println("Минимальная зарплата: " + salary.minSalary(employee) + " руб.");
         }
         {
-            System.out.println("Средняя зарплата: " + salary.averageSum(name) + " руб.");
+            System.out.println("Средняя зарплата: " + salary.averageSum(employee) + " руб.");
         }
 
     }
